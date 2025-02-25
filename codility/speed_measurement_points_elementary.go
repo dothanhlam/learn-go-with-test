4fb485ee297ffc868f99c7610734e143347e4f8c
aslan <dothanhlam@gmail.com>
2025-02-25 16:59:04 +0700

    speed measurement test

--- codility/speed_measurement_points_elementary.go
+++ codility/speed_measurement_points_elementary.go
@@ -1,1 +1,31 @@
 package codility
+
+func SpeedMeasurementPointElementary(S string) int {
+	n := len(S)
+	right := make([]int, n) // Create a slice (dynamically sized array)
+	left := make([]int, n)  // of integers, initialized to 0
+	rCars := 0
+	lCars := 0
+
+	for i := 0; i < n; i++ {
+		if S[i] == '.' {
+			right[i] = rCars
+		} else if S[i] == '>' {
+			rCars++
+		}
+	}
+
+	for i := n - 1; i >= 0; i-- {
+		if S[i] == '.' {
+			left[i] = lCars
+		} else if S[i] == '<' {
+			lCars++
+		}
+	}
+
+	ans := 0
+	for i := 0; i < n; i++ {
+		ans += left[i] + right[i]
+	}
+	return ans
+}
--- codility/speed_measurement_points_elementary_test.go
+++ codility/speed_measurement_points_elementary_test.go
@@ -3,9 +3,21 @@ package codility
 import "testing"
 
 func SpeedMeasurementPointElementaryTests(t *testing.T) {
-	t.Run("speed measurement points", func(t *testing.T) {
-		got := 0
-		want := 0
+	t.Run("Given S = '.>...', the function should return 3. The car will pass by three speed cameras to the right of it.", func(t *testing.T) {
+		got := SpeedMeasurementPointElementary(".>...")
+		want := 3
+		assertEqual(t, got, want)
+	})
+
+	t.Run("Given S = '.>.<.>', the function should return 4. The first two cars will pass by two speed cameras each, and the third car will not pass by any.", func(t *testing.T) {
+		got := SpeedMeasurementPointElementary(".>.<.>")
+		want := 4
+		assertEqual(t, got, want)
+	})
+
+	t.Run("Given S = '>>>.<<<', the function should return 6. Each car will pass one speed camera.", func(t *testing.T) {
+		got := SpeedMeasurementPointElementary(">>>.<<<")
+		want := 6
 		assertEqual(t, got, want)
 	})
 }
